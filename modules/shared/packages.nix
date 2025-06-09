a2b61a8e07d155213341c57abdfbc3d7cb9b625c
柚子 <40852301+uiYzzi@users.noreply.github.com>
2025-06-10 00:57:17 +0800

    refactor(shared/packages): 移除不再使用的maple-mono字体包

--- modules/shared/packages.nix
+++ modules/shared/packages.nix
@@ -28,11 +28,4 @@ with pkgs; [
 
   # Android
   android-tools
-
-  # Maple Mono (Ligature TTF unhinted)
-  maple-mono.truetype
-  # Maple Mono NF (Ligature unhinted)
-  maple-mono.NF-unhinted
-  # Maple Mono NF CN (Ligature unhinted)
-  maple-mono.NF-CN-unhinted
 ]
